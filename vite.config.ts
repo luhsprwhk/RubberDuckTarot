1fb0d45dee4a60cef50c9dc170aabd49bca9103a
Luh Sprwhk <06.askew.media@icloud.com>
2025-07-05 17:57:36 -0500

    feat: add vite-plugin-image-optimizer for image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer()],
   resolve: {
     alias: {
       '@': resolve(__dirname, '.'),
